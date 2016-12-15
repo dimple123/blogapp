09bb414fb5a9ac54eccab7ced4e9b11322921717
dimpal <dimpal.patel@einfochips.com>
2016-12-15 12:00:09 +0530

    commit ror project

---
+++ hellblog/db/migrate/20161214100202_create_recipes.rb
@@ -0,0 +1,11 @@
+class CreateRecipes < ActiveRecord::Migration[5.0]
+  def change
+    create_table :recipes do |t|
+      t.string :tittle
+      t.text :instructions
+      t.boolean :status
+
+      t.timestamps
+    end
+  end
+end
